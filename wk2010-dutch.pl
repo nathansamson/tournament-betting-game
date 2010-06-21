afb274666a1e0b3e60c51852c7de641bfad99a9c
Nathan Samson <nathansamson@gmail.com>
2010-06-21 17:48:42 +0200

    Add example of result set (wk 2010, dutch countrynames)

---
+++ wk2010-dutch.pl
@@ -0,0 +1,66 @@
+% A, first round
+match("Zuid-Afrika", "Mexico", 1, 1, 1).
+match("Uruguay", "Frankrijk", 1, 0, 0).
+match("Zuid-Afrika", "Uruguay", 1, 0, 3).
+match("Frankrijk", "Mexico", 1, 0, 2).
+%match("Mexico", "Uruguay", 1, ?, ?).
+%match("Frankrijk", "Zuid-Afrika", 1, ?, ?).
+
+% B, first round
+match("Zuid-Korea", "Griekenland", 1, 2, 0).
+match("Argentinïe", "Nigeria", 1, 1, 0).
+match("Argentinië", "Zuid-Korea", 1, 4, 1).
+match("Griekenland", "Nigeria", 1, 2, 1).
+%match("Nigeria, "Griekenland", 1, ?, ?).
+%match("Griekenland", "Argentinië", 1, ?, ?).
+
+% C, First round
+
+match("Engeland", "VS", 1, 1, 1).
+match("Algerije", "Slovenië", 1, 0, 1).
+match("Slovenë", "VS", 1, 2, 2).
+match("Engeland", "Algerije", 1, 0, 0).
+%match("Slovenië", "Engeland", 1, ?, ?).
+%match("VS", "Algerije", 1, ?, ?).
+
+% D
+match("Servië", "Ghana", 1, 0, 1).
+match("Duitsland", "Australië", 1, 4, 0).
+match("Duitsland", "Servië", 1, 0, 1).
+match("Ghana", "Australië", 1, 1, 1).
+%match("Ghana", "Duitsland", 1, ?, ?).
+%match("Australië", "Servië", 1, ?, ?).
+
+% E
+match("Nederland", "Denemarken", 1, 2, 0).
+match("Japan", "Kameroen", 1, 1, 0).
+match("Nederland", "Japan", 1, 1, 0).
+match("Kameroen", "Denemarken", 1, 1, 2).
+%match("Denemarken", "Japan", 1, ?, ?).
+%match("Kameroen", "Nederland", 1, ?, ?).
+
+% F
+match("Italië", "Paraguay", 1, 1, 1).
+match("Nieuw-Zeeland", "Slowakije", 1, 1, 1).
+match("Slowakije", "Paraguay", 1, 0, 2).
+match("Italiê", "Nieuw-Zeeland", 1, 1, 1).
+%match("Slowakije", "Italië", 1, ?, ?).
+%match("Paraguay", "Nieuw-Zeeland", 1, ?, ?).
+
+% G
+match("Ivoorkust", "Portugal", 1, 0, 0).
+match("Brazilië", "Noord-Korea", 1, 2, 1).
+match("Brazilië", "Ivoorkust", 1, 3, 1).
+match("Portugal", "Noord-Korea", 1, 7, 0).
+%match("Noord-Korea", "Ivoorkust", 1, ?, ?).
+%match("Portugal", "Brazilië", 1, ?, ?).
+
+
+% H
+match("Chili", "Zwitserland", 1, 1, 0).
+match("Honduras", "Chili", 1, 0, 1).
+match("Spanje", "Zwitserland", 1, 0, 1).
+%match("Spanje", "Honduras", 1, 7, 0).
+%match("Zwitserland", "Honduras", 1, ?, ?).
+%match("Chili", "Spanje", 1, ?, ?).
+
